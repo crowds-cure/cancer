74fba7f5547c4bec236bd23162258248a0adc0bb
André Botelho Almeida <almeidaa@ae.com>
2017-07-19 09:20:15 -0300

    Adding babel

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
 const rename = require('gulp-rename');
 const browserify = require('gulp-browserify');
+const babel = require('gulp-babel');
 const config = require('../config');
 
 gulp.task('build', function () {
   return gulp.src(config.entry)
     .pipe(browserify())
+    .pipe(babel({ presets: ['es2015'] }))
     .pipe(rename(config.buildFileJsName))
     .pipe(gulp.dest(config.dist));
 });
