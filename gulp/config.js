061ab053699eeb0796f942191d772699ddefe5de
Erik Ziegler <erik.sweed@gmail.com>
2018-10-03 16:16:20 +0200

    chore(build): Try to fix build so continuous deployment will work

--- gulp/config.js
+++ gulp/config.js
@@ -1,9 +1,10 @@
 module.exports = {
   entry: 'src/login/startLogin.js',
-  dist: './',
+  dist: './public',
   src: './src',
   sassEntry: './src/main.scss',
   buildFileJsName: 'bundle.js',
   buildFileCSSName: 'style.css',
+  html: '*.html',
   imageLoaderDistFiles: 'node_modules/cornerstone-wado-image-loader/dist/*',
 };
--- gulp/tasks/copyImageLoaderDistFiles.js
+++ gulp/tasks/copyImageLoaderDistFiles.js
@@ -3,5 +3,11 @@ const config = require('../config');
 
 gulp.task('copy:dist', function () {
   return gulp.src(config.imageLoaderDistFiles)
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./public/dist'));
+});
+
+// TODO: This is only temporary
+gulp.task('copy:html', function () {
+  return gulp.src(config.html)
+    .pipe(gulp.dest('./public/'));
 });
