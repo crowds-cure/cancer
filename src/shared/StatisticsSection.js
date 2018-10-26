fa836658b97fb476c531aa9f25828dd9913815e6
Gustavo Lelis <galelis@gmail.com>
2018-10-26 03:00:52 -0300

    Statistics Section component

--- src/shared/StatisticsSection.js
+++ src/shared/StatisticsSection.js
@@ -1,12 +1,29 @@
 import { Component } from 'react';
 import React from 'react';
 import StatisticsCard from './StatisticsCard.js';
+import './StatisticsSection.css';
 
 class StatisticsSection extends Component {
   render() {
     const userBadges = [
       {
         type: 'NUM_CASES_ROOKIE',
+        dateReached: new Date()
+      },
+      {
+        type: 'NUM_CASES_ROOKIE2',
+        dateReached: new Date()
+      },
+      {
+        type: 'NUM_CASES_ROOKIE3',
+        dateReached: new Date()
+      },
+      {
+        type: 'NUM_CASES_ROOKIE4',
+        dateReached: new Date()
+      },
+      {
+        type: 'NUM_CASES_ROOKIE5',
         dateReached: new Date()
       }
     ];
@@ -15,7 +32,12 @@ class StatisticsSection extends Component {
       <StatisticsCard key={item.type} type={item.type} />
     ));
 
-    return <div className="StatisticsSection">{items}</div>;
+    return (
+      <div className="StatisticsSection">
+        <span className="subTitle">Community stats</span>
+        {items}
+      </div>
+    );
   }
 }
 
