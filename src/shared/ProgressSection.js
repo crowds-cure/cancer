f4fd37135a1d260f7861018aa43868dc2b5e1873
Gustavo André Lelis <galelis@gmail.com>
2018-10-29 09:13:18 -0300

    feat(styling): Style progress section component (#12)
    * Progress Section component
    * Medal icon

--- src/shared/ProgressSection.js
+++ src/shared/ProgressSection.js
@@ -3,6 +3,7 @@ import React from 'react';
 import ProgressBar from './ProgressBar.js';
 import './ProgressSection.css';
 import PropTypes from 'prop-types';
+import medal from '../images/medal.svg';
 
 class ProgressSection extends Component {
   constructor(props) {
@@ -27,15 +28,25 @@ class ProgressSection extends Component {
 
     return (
       <div className="ProgressSection">
-        <span className="title">Your activity</span>
-        <ProgressBar
-          min={low}
-          max={high}
-          current={this.props.current}
-          increment={increment}
-        />
-        <span className="value">{this.props.current}</span>
-        <span className="suffix">cases</span>
+        <div>
+          <div className="numberCases">
+            <div className="medalContainer">
+              <div className="medalBackground">
+                <img src={medal} className="medal" alt="medal" />
+              </div>
+            </div>
+            <div className="currentPoints">
+              <span className="value">{this.props.current}</span>
+              <span className="suffix">cases</span>
+              <span className="plusPoints">+{increment}</span>
+            </div>
+          </div>
+          <div className="progressBarContainer">
+            <span className="progressLow">{low}</span>
+            <span className="progressHigh">{high}</span>
+            <ProgressBar min={low} max={high} current={this.props.current} increment={increment}/>
+          </div>
+        </div>
       </div>
     );
   }
