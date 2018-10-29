099a30890bd814fa6ce5208f7c3fb85e1af3a56a
Gustavo Lelis <galelis@gmail.com>
2018-10-29 20:08:48 -0300

    Moving Medal to its own component

---
+++ src/shared/Medal.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import React from 'react';
+import medal from '../images/medal.svg';
+import './Medal.css';
+
+class Medal extends Component {
+  render() {
+    const medalClassSize = `medal-bg-${this.props.size}`;
+
+    return (
+      <div className={medalClassSize}>
+        <img src={medal} className="medal" alt="medal" />
+      </div>
+    );
+  }
+}
+
+export default Medal;
