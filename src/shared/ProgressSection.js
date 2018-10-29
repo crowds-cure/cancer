a5187099bb582987a87af9311b580d8a4aca0e67
Gustavo Lelis <galelis@gmail.com>
2018-10-29 02:16:22 -0300

    Progress Section component

--- src/shared/ProgressSection.js
+++ src/shared/ProgressSection.js
@@ -2,6 +2,7 @@ import { Component } from 'react';
 import React from 'react';
 import ProgressBar from './ProgressBar.js';
 import './ProgressSection.css';
+import medal from '../images/medal.svg';
 
 class ProgressSection extends Component {
   render() {
@@ -11,10 +12,25 @@ class ProgressSection extends Component {
 
     return (
       <div className="ProgressSection">
-        <span className="title">Your activity</span>
-        <ProgressBar min={low} max={high} current={current} />
-        <span className="value">{current}</span>
-        <span className="suffix">cases</span>
+        <div>
+          <div className="numberCases">
+            <div className="medalContainer">
+              <div className="medalBackground">
+                <img src={medal} className="medal" alt="medal" />
+              </div>
+            </div>
+            <div className="currentPoints">
+              <span className="value">{current}</span>
+              <span className="suffix">cases</span>
+              <span className="plusPoints">+30</span>
+            </div>
+          </div>
+          <div className="progressBarContainer">
+            <span className="progressLow">{low}</span>
+            <span className="progressHigh">{high}</span>
+            <ProgressBar min={low} max={high} current={current} />
+          </div>
+        </div>
       </div>
     );
   }
