22cca3bd4d8b430760dfa70675835e17622f6ed4
Gustavo Lelis <galelis@gmail.com>
2018-10-31 05:21:15 -0300

    Adding Layout breakpoints

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -31,45 +31,47 @@ class Dashboard extends Component {
     const current = this.state.userStats.current;
 
     return (
-      <div className="Dashboard container">
-        <div className="row">
-          <div className="col">
-            <SimpleHeaderSection />
-            {/* These will be styled soon */}
-            <span className="Logout">{this.props.username} </span>
-            <span className="Logout" onClick={this.logout}>
-              Log out
-            </span>
+      <div className="Dashboard">
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <SimpleHeaderSection />
+              {/* These will be styled soon */}
+              <span className="Logout">{this.props.username} </span>
+              <span className="Logout" onClick={this.logout}>
+                Log out
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-5 offset-lg-4 col-md-7 offset-md-2">
-            <ActivityProgressSection current={current} />
-            {/*<AchievementSection />*/}
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-lg-4 col-md-16 order-2 order-lg-1">
+          <div className="row">
+            <div className="col-lg-5 offset-lg-4 col-md-7 offset-md-2">
+              <ActivityProgressSection current={current} />
+              {/*<AchievementSection />*/}
+            </div>
             <div className="row">
-              <div className="col-lg-16 col-md-8 col-sm-16">
-                <StatisticsSection col="8" />
-              </div>
-              <div className="col-lg-16 col-md-8 col-sm-16">
+              <div className="col-lg-4 col-md-16 order-2 order-lg-1">
                 <div className="row">
-                  <div className="col-16">
-                    <RankingSection name="Close to you" />
+                  <div className="col-lg-16 col-md-8 col-sm-16">
+                    <StatisticsSection col="8" />
                   </div>
-                  <div className="col-16">
-                    <RankingSection name="Top this week" />
+                  <div className="col-lg-16 col-md-8 col-sm-16">
+                    <div className="row">
+                      <div className="col-16">
+                        <RankingSection name="Close to you" />
+                      </div>
+                      <div className="col-16">
+                        <RankingSection name="Top this week" />
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          </div>
-          <div className="col-lg-12 col-md-16 col-sm-16 order-1 order-lg-2">
-            <div className="row">
-              <div className="col-16">
-                <CaseTypeSection />
+              <div className="col-lg-12 col-md-16 col-sm-16 order-1 order-lg-2">
+                <div className="row">
+                  <div className="col-16">
+                    <CaseTypeSection />
+                  </div>
+                </div>
               </div>
             </div>
           </div>
