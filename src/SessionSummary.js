5ccdd90a718e4eab10f9abab6dc5bd2435056b0a
Gustavo André Lelis <galelis@gmail.com>
2018-11-01 03:50:28 -0300

    feat(viewer): Adding Layout breakpoints (#17)

--- src/SessionSummary.js
+++ src/SessionSummary.js
@@ -12,40 +12,43 @@ import SimpleHeaderSection from './shared/SimpleHeaderSection';
 class SessionSummary extends Component {
   render() {
     return (
-      <div className="SessionSummary container">
-        <div className="row align-items-start">
-          <div className="col">
-            <SimpleHeaderSection />
+      <div className="SessionSummary">
+        <div className="container">
+          <div className="row align-items-start">
+            <div className="col">
+              <SimpleHeaderSection />
+            </div>
           </div>
-        </div>
-        <div className="row align-items-center">
-          <div className="col title-wrapper">
-            <label className="title">SESSION SUMMARY</label>
+          <div className="row align-items-center">
+            <div className="col title-wrapper">
+              <label className="title">SESSION SUMMARY</label>
+            </div>
           </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-10">
-            <ProgressSection
-              current={this.props.current}
-              numCasesInSession={this.props.numCasesInSession} />
+          <div className="row justify-content-center">
+            <div className="col-10">
+              <ProgressSection
+                current={this.props.current}
+                numCasesInSession={this.props.numCasesInSession}
+              />
+            </div>
           </div>
-        </div>
-        <div className="row justify-content-center">
-          <div className="col-6">
-            <LogoutSection />
+          <div className="row justify-content-center">
+            <div className="col-6">
+              <LogoutSection />
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="col-10 communityStats">
-            <StatisticsSection />
-          </div>
-          <div className="col-6 rankingStats">
-            <div className="row">
-              <div className="col">
-                <RankingSection name="Close to you" />
-              </div>
-              <div className="col">
-                <RankingSection name="Top this week" />
+          <div className="row">
+            <div className="col-10 communityStats">
+              <StatisticsSection />
+            </div>
+            <div className="col-6 rankingStats">
+              <div className="row">
+                <div className="col">
+                  <RankingSection name="Close to you" />
+                </div>
+                <div className="col">
+                  <RankingSection name="Top this week" />
+                </div>
               </div>
             </div>
           </div>
