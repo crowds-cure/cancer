d84f06a9ad7098f4acdfba22710d28fe5ee8f03a
Evren Ozkan <evren217@gmail.com>
2018-11-14 15:12:44 -0500

    feat(achievements): Show achievements with mobile responsivity in dashboard

---
+++ src/achievements.js
@@ -0,0 +1,24 @@
+import testBadge from './images/test-badge.svg';
+
+const achievements = {
+  TestBadge1: {
+    img: testBadge
+  },
+  TestBadge2: {
+    img: testBadge
+  },
+  TestBadge3: {
+    img: testBadge
+  },
+  TestBadge4: {
+    img: testBadge
+  },
+  TestBadge5: {
+    img: testBadge
+  },
+  TestBadge6: {
+    img: testBadge
+  }
+};
+
+export { achievements };
