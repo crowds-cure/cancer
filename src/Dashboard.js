51f7a36a6b8601af6370826e11f94b0597f1f3f0
Evren Ozkan <evren217@gmail.com>
2018-11-16 17:51:18 -0500

    feat(achievements): Add achievement badge images and description displayed in tooltip when mouse hovers

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -10,6 +10,7 @@ import AchievementSection from './shared/AchievementSection.js';
 
 import './Dashboard.css';
 import Modal from 'react-modal';
+import ReactTooltip from 'react-tooltip';
 import { getDB } from './db';
 import getUserStats from './shared/getUserStats';
 import getUsername from './viewer/lib/getUsername.js';
@@ -170,6 +171,7 @@ class Dashboard extends Component {
             Close
           </span>
         </Modal>
+        <ReactTooltip className="DashboardTooltip" effect="solid" />
       </div>
     );
   }
