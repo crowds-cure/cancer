bd949afad45e3583311b8696186ed0116661bba8
Evren Ozkan <evren217@gmail.com>
2018-11-16 20:08:11 -0500

    fix(rank-badges): Pass rank badge image from the progress section to rank badge component

--- src/shared/RankBadge.js
+++ src/shared/RankBadge.js
@@ -1,14 +1,12 @@
 import { Component } from 'react';
 import React from 'react';
-import { BADGE_TYPES } from '../badges.js';
 import './RankBadge.css';
 
 class RankBadge extends Component {
   render() {
-    const rankImg = BADGE_TYPES[this.props.type].img;
     return (
-      <div className="rankBadge" title={this.props.type}>
-        <img src={rankImg} alt="Rank Badge" />
+      <div className="rankBadge" title={this.props.name}>
+        <img src={this.props.img} alt={this.props.name} />
       </div>
     );
   }
