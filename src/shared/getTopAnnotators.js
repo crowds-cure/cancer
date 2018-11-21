89ee7319b5a6cb71c36fbe3b230073e2c5be6991
Evren Ozkan <evren217@gmail.com>
2018-11-20 22:59:53 -0500

    fix(leaderboard): Do not limit results in getTopAnnotatorsByWeek and getTopTeamsByWeek functions

--- src/shared/getTopAnnotators.js
+++ src/shared/getTopAnnotators.js
@@ -102,7 +102,7 @@ async function getTopAnnotatorsByWeek(week) {
 
   console.log(`top for week ${weekKey} annotators: `, annotators);
 
-  return annotators.slice(0, 3);
+  return annotators;
 }
 
 // return the top teams for the given week, where
@@ -163,7 +163,7 @@ async function getTopTeamsByWeek(week) {
 
   console.log(`top for teams week ${weekKey} teams: `, topTeams);
 
-  return topTeams.slice(0, 3);
+  return topTeams;
 }
 
 export { getTopAnnotators, getTopAnnotatorsByWeek, getTopTeamsByWeek };
