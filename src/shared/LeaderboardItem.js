30b01a7cfd27b0da13b9415eeffef402c51bc1f4
Evren Ozkan <evren217@gmail.com>
2018-11-20 22:45:07 -0500

    feat(leaderboard): Add Leaderboard UI component

---
+++ src/shared/LeaderboardItem.js
@@ -0,0 +1,26 @@
+import { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import './LeaderboardItem.css';
+
+class LeaderboardItem extends Component {
+  render() {
+    return (
+      <div className="LeaderboardItem">
+        <div className="score">
+          {this.props.score && this.props.score.toLocaleString()}
+        </div>
+        <div className="rank">{this.props.rank}</div>
+        <div className="name">{this.props.name}</div>
+      </div>
+    );
+  }
+}
+
+LeaderboardItem.propTypes = {
+  score: PropTypes.number,
+  rank: PropTypes.number,
+  name: PropTypes.string
+};
+
+export default LeaderboardItem;
