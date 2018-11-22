0a2eb5d2685f1551427d0729a9b00c9429be70b0
Erik Ziegler <erik.sweed@gmail.com>
2018-11-22 20:20:19 +0100

    fix(leaderboard): Reorder leaderboard so it's more clear

--- src/shared/LeaderboardItem.js
+++ src/shared/LeaderboardItem.js
@@ -7,12 +7,12 @@ class LeaderboardItem extends Component {
   render() {
     return (
       <div className="LeaderboardItem">
-        <div className="score">
-          {this.props.score && this.props.score.toLocaleString()}
-        </div>
         <div className="rank">{this.props.rank}</div>
         <div className="name" title={this.props.name}>
           {this.props.name}
+        </div>
+        <div className="score">
+          {this.props.score && this.props.score.toLocaleString()}
         </div>
       </div>
     );
