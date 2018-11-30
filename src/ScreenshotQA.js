d4f4ddbc883cbc8cbe40bdfefe360b7d5d8a14e3
Gustavo Lelis <galelis@gmail.com>
2018-11-30 03:54:43 -0200

    feat(screenshotQA): Adding pagination and user selection

--- src/ScreenshotQA.js
+++ src/ScreenshotQA.js
@@ -1,22 +1,61 @@
 import { Component } from 'react';
 import React from 'react';
-import SimpleHeaderSection from './shared/SimpleHeaderSection';
+import ReactPaginate from 'react-paginate';
+
 import { getDB } from './db.js';
-import getAuthorizationHeader from './openid-connect/getAuthorizationHeader.js';
+import SimpleHeaderSection from './shared/SimpleHeaderSection';
 import SecuredImage from './shared/SecuredImage.js';
+import getAuthorizationHeader from './openid-connect/getAuthorizationHeader.js';
+
 import './ScreenshotQA.css';
 
-async function getLatestMeasurements() {
-  const measurementsDB = getDB('measurements');
+async function getAnnotators(measurementsDB) {
+  const result = await measurementsDB.query('by/annotatorsIDs');
+  const annotators = [];
 
-  const result = await measurementsDB.find({
+  if (!result.rows || !result.rows[0]) {
+    return annotators;
+  }
+
+  result.rows.forEach(row => {
+    if (!annotators.includes(row.value) && row.value !== null) {
+      annotators.push(row.value);
+    }
+  });
+
+  return annotators;
+}
+
+async function getTotalMeasurements(measurementsDB, annotator) {
+  const result = await measurementsDB.query('by/annotators', {
+    selector: {
+      annotator: annotator
+    }
+  });
+
+  if (!result.rows || !result.rows[0]) {
+    return 0;
+  }
+
+  return result.rows[0].value;
+}
+
+async function getMeasurements(measurementsDB, options = {}) {
+  let { limit = 25, skip = 0, sortDate = 'desc', annotator } = options;
+  const queryOptions = {
     selector: {
       skip: false
     },
     fields: ['_id', 'date', 'annotator', 'caseData', 'feedback'],
-    sort: [{ date: 'desc' }],
-    limit: 25
-  });
+    sort: [{ date: sortDate }],
+    limit: limit,
+    skip: skip
+  };
+  if (annotator) {
+    queryOptions.selector['annotator'] = annotator;
+  }
+
+  const result = await measurementsDB.find(queryOptions);
 
   return result.docs;
 }
@@ -70,36 +109,127 @@ const options = [
 ];
 
 class ScreenshotQA extends Component {
+  static defaultProps = {
+    screenshotPerPage: 10
+  };
+
   constructor(props) {
     super(props);
 
     this.state = {
-      measurements: []
+      pageNumber: 0,
+      totalMeasurements: 0,
+      userMeasurements: 0,
+      screenshotPerPage: this.props.screenshotPerPage,
+      measurements: [],
+      annotator: null
     };
 
-    this.updateMeasurements = this.updateMeasurements.bind(this);
-
+    this.measurementsDB = getDB('measurements');
+    getAnnotators(this.measurementsDB).then(annotators => {
+      this.annotators = annotators;
+    });
     this.updateMeasurements();
   }
 
+  render() {
+    debugger;
+    const { totalMeasurements, screenshotPerPage } = this.state;
+    const pageCount = Math.ceil(totalMeasurements / screenshotPerPage) || 0;
+    return (
+      <div className="ScreenshotQA">
+        <SimpleHeaderSection />
+        <div className="controlHeader">
+          <ReactPaginate
+            previousLabel={'previous'}
+            nextLabel={'next'}
+            pageCount={pageCount}
+            marginPagesDisplayed={4}
+            pageRangeDisplayed={4}
+            onPageChange={this.handlePageClick}
+            containerClassName={'pagination'}
+            subContainerClassName={'pages pagination'}
+            activeClassName={'active'}
+          />
+          {this.annotators && (
+            <div className="usernameField">
+              <label htmlFor="username">Username search:</label>
+              <select id="username" onChange={this.annoratorSelected}>
+                <option value={null}>No annotator selected</option>
+                {this.getAnnotatorsOptions()}
+              </select>
+            </div>
+          )}
+        </div>
+        <div className="container">
+          <div className="row justify-content-center">
+            <div className="col-sm-16">{this.getScreenshots()}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   componentDidMount() {
-    this.interval = setInterval(this.updateMeasurements, 20000);
+    //this.interval = setInterval(this.updateMeasurements, 20000);
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+    //clearInterval(this.interval);
   }
 
-  updateMeasurements() {
-    getLatestMeasurements().then(measurements => {
-      this.setState({
-        measurements
+  handlePageClick = page => {
+    this.updateMeasurements(page.selected);
+  };
+
+  getAnnotatorsOptions = () => {
+    if (this.annotators) {
+      return this.annotators.map((annotator, index) => {
+        return (
+          <option key={index} value={annotator}>
+            {annotator}
+          </option>
+        );
       });
-    });
-  }
+    }
+  };
 
-  render() {
-    const screenshots = this.state.measurements.map(measurement => {
+  annoratorSelected = evt => {
+    const goToPageNumber = this.state.pageNumber;
+    const annotator = evt.target.value;
+    this.updateMeasurements(goToPageNumber, annotator);
+  };
+
+  updateMeasurements = async (goToPageNumber, annotator) => {
+    const { pageNumber, screenshotPerPage } = this.state;
+    let totalMeasurements = this.state.totalMeasurements;
+    goToPageNumber = goToPageNumber || pageNumber;
+    const skip = goToPageNumber * screenshotPerPage;
+
+    const measurements = await getMeasurements(this.measurementsDB, {
+      annotator: annotator,
+      limit: screenshotPerPage,
+      skip
+    });
+
+    if (totalMeasurements === 0 || annotator) {
+      totalMeasurements = await getTotalMeasurements(
+        this.measurementsDB,
+        annotator
+      );
+    }
+
+    debugger;
+    this.setState({
+      totalMeasurements,
+      measurements,
+      pageNumber,
+      annotator
+    });
+  };
+
+  getScreenshots = () => {
+    return this.state.measurements.map(measurement => {
       const host = 'https://db.crowds-cure.org';
       const db = 'measurements';
       const imgName = 'screenshot.jpeg';
@@ -143,18 +273,7 @@ class ScreenshotQA extends Component {
         </div>
       );
     });
-
-    return (
-      <div className="ScreenshotQA">
-        <SimpleHeaderSection />
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-sm-16">{screenshots}</div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  };
 }
 
 export default ScreenshotQA;
