23f0ddf9987f678a7979bd2c103bf84f17afd520
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2019-09-17 19:15:19 -0300

    Updating the header layout and making it responsive

--- src/shared/SimpleHeaderSection.js
+++ src/shared/SimpleHeaderSection.js
@@ -1,23 +1,37 @@
 import { Component } from 'react';
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './SimpleHeaderSection.css';
 import Logo from './Logo.js';
+import iconUserDefault from '../images/general/icon-user-default.svg';
 
 class SimpleHeaderSection extends Component {
   render() {
     const isDashboard = this.props.page === 'dashboard';
-    const logoutButtons = (
-      <div className="logoutSection">
-        <span className="username">{this.props.username}</span>
-        <span className="logoutButton" onClick={this.logout}>
-          Logout
-        </span>
-      </div>
+    const infoSection = (
+      <>
+        <div className="userSection">
+          <img
+            className="userIcon"
+            src={iconUserDefault}
+            alt={this.props.username}
+          />
+          <span className="username">{this.props.username}</span>
+        </div>
+        <div className="actionSection">
+          <Link to={this.props.leaderboardLink} className="leaderboardButton">
+            Leaderboard
+          </Link>
+          <span className="logoutButton" onClick={this.logout}>
+            Logout
+          </span>
+        </div>
+      </>
     );
     return (
       <div className="simpleHeader">
         <Logo />
-        {isDashboard && logoutButtons}
+        {isDashboard && infoSection}
       </div>
     );
   }
