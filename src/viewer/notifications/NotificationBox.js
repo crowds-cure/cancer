f310c1f56104839fa17d0cde1b17e1db1786dc47
Bruno Alves de Faria <brunoalvesdefaria@gmail.com>
2019-10-01 12:41:55 -0300

    Creating badge notification and adding slideIn animation

---
+++ src/viewer/notifications/NotificationBox.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './NotificationBox.css';
+
+import halo1 from '../../images/general/halo1.svg';
+import halo2 from '../../images/general/halo2.svg';
+import halo3 from '../../images/general/halo3.svg';
+
+class NotificationBox extends React.Component {
+  static propTypes = {
+    title: PropTypes.node,
+    message: PropTypes.node,
+    icon: PropTypes.node
+  };
+
+  static defaultProps = {
+    title: null,
+    message: PropTypes.node,
+    icon: null
+  };
+
+  render() {
+    const { title, message, icon } = this.props;
+    return (
+      <>
+        <div className="icon">
+          <img src={icon} alt={title} />
+          <div className="halo halo1">
+            <img src={halo1} alt={title} />
+          </div>
+          <div className="halo halo2">
+            <img src={halo2} alt={title} />
+          </div>
+          <div className="halo halo3">
+            <img src={halo3} alt={title} />
+          </div>
+        </div>
+        <div className="notificationText" role="alert">
+          {message ? <div className="message">{message}</div> : ''}
+          {title ? <h4 className="title">{title}</h4> : ''}
+        </div>
+      </>
+    );
+  }
+}
+
+export default NotificationBox;
