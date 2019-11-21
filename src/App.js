0935dd1608c3cda286b785a9c97a1adac2c2b8c4
Rodolfo Ladeira <rodolfo2611@gmail.com>
2019-11-21 14:37:01 -0300

    Remove switch package and do control transition with cssTransition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router';
-import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import SecretRoute from './SecretRoute.js';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import { CSSTransition } from 'react-transition-group';
 import './App.css';
 import './AppPageAnimations.css';
 import './grid-16.css';
+
+import Auth from './openid-connect/Auth.js';
+import LoadingIndicator from './shared/LoadingIndicator.js';
 
 import Rollbar from './shared/ErrorHandling.js';
 import ConnectedDashboard from './ConnectedDashboard.js';
@@ -19,7 +21,7 @@ import LogoutTimeoutService from './LogoutTimeoutService.js';
 
 const reload = () => window.location.reload();
 
-function setContext(context) {
+function setRollbarContext(context) {
   Rollbar.configure({
     payload: {
       context
@@ -27,97 +29,115 @@ function setContext(context) {
   });
 }
 
+const routes = [
+  // can combine paths into array once react-router-dom is at least v4.4.x (or v5.0.0 as per distributor)
+  { path: '/dashboard', name: 'dashboard', Component: ConnectedDashboard },
+  { path: '/', name: 'home', Component: ConnectedDashboard },
+  { path: '/viewer', name: 'viewer', Component: ConnectedViewer },
+  {
+    path: '/session-summary',
+    name: 'viewer',
+    Component: ConnectedSessionSummary
+  },
+  { path: '/screenshot-qa', name: 'viewer', Component: ScreenshotQA },
+  { path: '/leaderboard', name: 'viewer', Component: Leaderboard },
+  { path: '/stats', name: 'viewer', Component: StatisticsPage }
+];
+
+const getComponent = (Component, props, componentsReady) => {
+  return componentsReady ? <Component {...props} /> : <LoadingIndicator />;
+};
+
+const firstPassAuthConfig = (props, setComponentsReadyCallback) => {
+  const { auth, store, location } = props;
+  const authenticated = auth.isAuthenticated();
+  const hasSignInResponse = Auth.urlHasSignInResponse();
+
+  if (!authenticated && !hasSignInResponse) {
+    const { pathname } = location;
+    auth.login({ redirect_uri: Auth.absoluteURL(pathname) });
+  } else if (!authenticated && hasSignInResponse) {
+    auth.handleAuthentication().then(async () => {
+      // User is Authenticated, update the Redux store
+      // with the user information
+      store.dispatch({
+        type: 'SET_FROM_DATABASE',
+        savedState: {
+          username: auth.profile.username,
+          occupation: auth.profile.occupation,
+          team: auth.profile.team,
+          experience: auth.profile.experience
+        }
+      });
+
+      setComponentsReadyCallback();
+    });
+  }
+};
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      componentsReady: false
+    };
+
+    this.historyListen = undefined;
+  }
+
   componentWillMount() {
-    console.warn('>>>>DID_MOUNT');
-    this.unlisten = this.props.history.listen((location, action) => {
-      console.warn('>>>>CAPTURED', location);
-      setContext(window.location.pathname);
+    this.historyListen = this.props.history.listen((location, action) => {
+      setRollbarContext(window.location.pathname);
+    });
+  }
+
+  componentDidMount() {
+    const { auth, store, location } = this.props;
+
+    firstPassAuthConfig({ auth, store, location }, () => {
+      this.setState({
+        componentsReady: true
+      });
     });
 
     LogoutTimeoutService.init();
   }
 
   componentWillUnmount() {
-    this.unlisten();
+    if (typeof this.historyListen === 'function') {
+      this.historyListen();
+    }
 
     LogoutTimeoutService.destroy();
   }
 
   render() {
-    const location = this.props.location;
-    const currentKey = location.pathname.split('/')[1] || '/';
+    const { auth, store } = this.props;
+    const { componentsReady } = this.state;
 
     return (
       <Router>
-        <TransitionGroup className="transition-group">
-          <CSSTransition
-            key={currentKey}
-            timeout={{ enter: 2000, exit: 1000 }}
-            classNames={'fade'}
-            appear
-          >
-            <section className="route-section">
-              <Switch location={location}>
-                <SecretRoute
-                  exact
-                  path="/dashboard"
-                  component={ConnectedDashboard}
-                  auth={this.props.auth}
-                  store={this.props.store}
-                />
-                <SecretRoute
-                  exact
-                  path="/"
-                  component={ConnectedDashboard}
-                  auth={this.props.auth}
-                  store={this.props.store}
-                />
-                <SecretRoute
-                  exact
-                  path="/viewer"
-                  component={ConnectedViewer}
-                  auth={this.props.auth}
-                  store={this.props.store}
-                />
-                <SecretRoute
-                  exact
-                  path="/session-summary"
-                  component={ConnectedSessionSummary}
-                  auth={this.props.auth}
-                  store={this.props.store}
-                />
-                <SecretRoute
-                  exact
-                  path="/screenshot-qa"
-                  component={ScreenshotQA}
-                  auth={this.props.auth}
-                  store={this.props.store}
-                />
-                <SecretRoute
-                  exact
-                  path="/leaderboard"
-                  component={Leaderboard}
-                  auth={this.props.auth}
-                  store={this.props.store}
-                />
-                <SecretRoute
-                  exact
-                  path="/stats"
-                  component={StatisticsPage}
-                  auth={this.props.auth}
-                  store={this.props.store}
-                />
-                <Route path="/silent-refresh.html" onEnter={reload} />
-                <Route path="/logout-redirect.html" onEnter={reload} />
-                {/*<Route exact path="/playground" component={TestPage} />*/}
-                <Route
-                  render={() => <div> Sorry, this page does not exist. </div>}
-                />
-              </Switch>
-            </section>
-          </CSSTransition>
-        </TransitionGroup>
+        <section className="route-section">
+          {routes.map(({ path, Component, HOC }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match !== null}
+                  timeout={{ enter: 2000, exit: 1000 }}
+                  classNames="fade"
+                  appear
+                  unmountOnExit
+                >
+                  {getComponent(Component, { auth, store }, componentsReady)}
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+          <Route path="/silent-refresh.html" onEnter={reload} />
+          <Route path="/logout-redirect.html" onEnter={reload} />
+          {/*<Route exact path="/playground" component={TestPage} />*/}
+          <Route render={() => <div> Sorry, this page does not exist. </div>} />
+        </section>
       </Router>
     );
   }
