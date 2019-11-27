e41656cf60bbeeb4dd3e7c2a0d4625270d9d46a8
Rodolfo Ladeira <rodolfo2611@gmail.com>
2019-11-27 17:14:46 -0300

    Prevent component rerender on every imagererender.

--- src/viewer/ViewportOverlay.js
+++ src/viewer/ViewportOverlay.js
@@ -6,19 +6,22 @@ import PropTypes from 'prop-types';
 
 class ViewportOverlay extends Component {
   render() {
-    // TODO: Round the value to 2 decimals
-    const scale = Math.round(this.props.viewport.scale * 100) / 100;
-    const imageId = this.props.imageId;
+    const {
+      roundedViewportScale,
+      imageId,
+      viewportVoi,
+      stack,
+      numImagesLoaded
+    } = this.props;
 
     const patientId = cornerstone.metaData.get('00100020', imageId);
     const studyDate = cornerstone.metaData.get('00080020', imageId);
     const collection = cornerstone.metaData.get('00131010', imageId);
     const studyDescription = cornerstone.metaData.get('00081030', imageId);
 
-    const windowWidth = Math.round(this.props.viewport.voi.windowWidth);
-    const windowCenter = Math.round(this.props.viewport.voi.windowCenter);
-    const imagesLeft =
-      this.props.stack.imageIds.length - this.props.numImagesLoaded;
+    const windowWidth = Math.round(viewportVoi.windowWidth);
+    const windowCenter = Math.round(viewportVoi.windowCenter);
+    const imagesLeft = stack.imageIds.length - numImagesLoaded;
 
     return (
       <div className="ViewportOverlay">
@@ -31,14 +34,15 @@ class ViewportOverlay extends Component {
             {imagesLeft > 0 ? `${imagesLeft} images remaining...` : ''}
           </span>
         </div>
-        <div className="bottom-left overlay-element">Zoom: {scale}</div>
+        <div className="bottom-left overlay-element">
+          Zoom: {roundedViewportScale}
+        </div>
         <div className="bottom-right overlay-element">
           <span>
             WW/WC: {windowWidth} / {windowCenter}
           </span>
           <span>
-            Image: {this.props.stack.currentImageIdIndex + 1} /{' '}
-            {this.props.stack.imageIds.length}
+            Image: {stack.currentImageIdIndex + 1} / {stack.imageIds.length}
           </span>
         </div>
       </div>
@@ -47,7 +51,8 @@ class ViewportOverlay extends Component {
 }
 
 ViewportOverlay.propTypes = {
-  viewport: PropTypes.object.isRequired,
+  viewportVoi: PropTypes.object.isRequired,
+  roundedViewportScale: PropTypes.number.isRequired,
   imageId: PropTypes.string.isRequired,
   stack: PropTypes.object.isRequired
 };
